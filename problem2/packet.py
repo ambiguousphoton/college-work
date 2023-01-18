f6ee79a6e12a065272f493a25c976b32b0a82343
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2023-01-18 23:58:03 +0530

    Add files via upload

---
+++ problem2/packet.py
@@ -0,0 +1,10 @@
+class packet:
+    def __init__(self ,p_name,client_name):
+        self.client_id = client_name
+        self.client_ip = "120.0.0.1"
+        self.destination = "120.0.0.1"
+        self.message = ""
+        self.Name_of_message = p_name
+        self.id = None
+        self.current_packet_id = None
+# jai bajrang bali  
