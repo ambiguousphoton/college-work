d7d49a310fd7fc5321c25a7ffcc185ed9dd731dd
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2023-01-19 00:00:34 +0530

    Delete reciever.py

--- reciever.py
+++
@@ -1,7 +0,0 @@
-class reciever:
-    def recieve(payload):
-        # print(payload)
-        main_message = ""
-        for i in payload:
-            main_message += i.message
-        print (main_message)
